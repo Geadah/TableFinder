b44fec88f72046f41ccf58cd3ba3c8e61f509b05
Geadah <arthur.b.diniz@hotmail.com>
2018-03-29 21:17:16 -0300

    tela que vai pra descricao do estabele
    dsadsadsadsadsa

---
+++ TableFinder/TableFinder.WebUI/Controllers/NavegacaoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TableFinder.WebUI.Controllers
+{
+    public class NavegacaoController : Controller
+    {
+        // GET: Navegacao
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}

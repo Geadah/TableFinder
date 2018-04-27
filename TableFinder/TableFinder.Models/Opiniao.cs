6c82d62b551bb734ad46b832b755ac993dc31b5b
ririhmatheus <ririhmatheus@gmail.com>
2018-04-26 21:18:12 -0300

    Tentativa de homicídio

---
+++ TableFinder/TableFinder.Models/Opiniao.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TableFinder.Models
+{
+    public class Opiniao
+    {
+        public int Id_Usuario { get; set; }
+        public int Id_post { get; set; }
+        public Cadastro Usuario { get; set; }
+        public DateTime Data_Hora { get; set; }
+        public string Post { get; set; }
+        public int Nota { get; set; }
+        
+    }
+}

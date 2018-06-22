6ca085aad522d10114f93faf9685a7cfe7bdff6c
Geadah <arthur.b.diniz@hotmail.com>
2018-06-21 21:12:22 -0300

    small changes

--- TableFinder/BancoDeDados.sql
+++ TableFinder/BancoDeDados.sql
@@ -47,7 +47,7 @@ values
 (
 	'Iguarias do Edemar',
 	'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-	'smalahove_just_eaten.jpg',
+	'costela-de-chao-de-forno-do-jimmy-31937.jpg',
 	'21.187.149/0001-00',
 	'Rua Padre Leonardo Nunes, 180 - Portão, Curitiba - PR, 80330-320',
 	'1',
